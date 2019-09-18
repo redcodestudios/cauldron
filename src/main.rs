c0658a9691bf4eca6ba945ebb93de2a86d455310
Pablo Diego <pablodiegoss@hotmail.com>
2019-09-18 15:46:49 -0300

    Remove struct to convert to bytes correctly
    Co-authored-by: "Rodrigo Oliveira <rodrigo.redcode@gmail.com>"

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ use std::net::UdpSocket;
 
 #[derive(Serialize, Deserialize)]
 struct DNSMessage {
-   header: Vec<u8>,
-   payload: Vec<u8>,
-   answers_pr: u32,
-   authority_rr: u32,
-   additional_pr: u32
+    header: Vec<u8>,
+    payload: Vec<u8>,
+    answers_pr: u32,
+    authority_rr: u32,
+    additional_pr: u32,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -22,14 +22,13 @@ struct HeaderSection {
     qdcount: u16,
     ancount: u16,
     nscount: u16,
-    arcount: u16
+    arcount: u16,
 }
 
 fn main() {
-
     if env::args().len() != 3 {
         println!("Invalid arguments!");
-    }else {
+    } else {
         if let Some(hostname) = env::args().nth(1) {
             if let Some(dns_server) = env::args().nth(2) {
                 get_ip(hostname, dns_server);
@@ -42,14 +41,34 @@ fn main() {
     // }
 }
 
-
-fn get_ip(cname: String, dns_server: String){
+fn get_ip(cname: String, dns_server: String) {
     let socket = UdpSocket::bind("0.0.0.0:34254").expect("couldn't bind to address");
     // socket.connect(dns_server+":5005").expect("couldn't connect to address");
-    let message = build_message(cname);
-    let bytes = bincode::serialize(&message).unwrap();
-    socket.send_to(&bytes, dns_server).expect("couldn't send data");
-    
+    // let message = build_message(cname);
+    let id = (0b00000000, 0b00000001);
+    let codes = (0b00000001, 0b00000000);
+    let question_qtd = (0b00000000, 0b00000001);
+    let mut bytes = vec![id.0, id.1 , codes.0,codes.1, question_qtd.0, question_qtd.1];
+
+    let labels = transform_cname(cname);
+
+    let one_flag: u16 = 0x0001;
+    let mut cname_bytes = bincode::serialize(&labels).unwrap();
+    let mut qtype_bytes = bincode::serialize(&one_flag).unwrap();
+    let mut qclass_bytes = bincode::serialize(&one_flag).unwrap();
+
+    let mut questions_bytes = vec![0; cname_bytes.len() + 4];
+
+    questions_bytes.append(&mut cname_bytes);
+    questions_bytes.append(&mut qtype_bytes);
+    questions_bytes.append(&mut qclass_bytes);
+
+    bytes.append(&mut questions_bytes);
+    // let bytes:Vec<u8> = bincode::serialize(&message).unwrap();
+   
+    socket
+        .send_to(&mut bytes, dns_server)
+        .expect("couldn't send data");
     let mut buf = [0; 1024];
     match socket.recv(&mut buf) {
         Ok(received) => println!("received {} bytes {:?}", received, &buf[..received]),
@@ -57,33 +76,32 @@ fn get_ip(cname: String, dns_server: String){
     }
 }
 
-
-fn build_message(cname: String) -> DNSMessage{
+fn build_message(cname: String) -> DNSMessage {
     // 0 0000 0 0 0 0 0000
-    let id = 666;
-    let codes = 256;
-    let qdcount = 1;
-    let ancount = 0;
-    let nscount = 0;
-    let arcount = 0;
+    let id: u16 = 0b0000000000000001;
+    let codes: u16 = 0b0000000100000000;
+    let qdcount: u16 = 0b0000000000000001;
+    let ancount: u16 = 0b0000000000000000;
+    let nscount: u16 = 0b0000000000000000;
+    let arcount: u16 = 0b0000000000000000;
 
-    let header = HeaderSection{
+    let header = HeaderSection {
         id: id,
         codes: codes,
-        qdcount: qdcount, 
-        ancount: ancount, 
+        qdcount: qdcount,
+        ancount: ancount,
         nscount: nscount,
-        arcount: arcount
+        arcount: arcount,
     };
 
-    let mut header_bytes = bincode::serialize(&header).unwrap();
+    let header_bytes = bincode::serialize(&header).unwrap();
     let labels = transform_cname(cname);
-    println!("{}",labels);
+    println!("{}", labels);
     // let question_bytes = transform_to_bytes(labels)
-
+    let one_flag: u16 = 0x0001;
     let mut cname_bytes = bincode::serialize(&labels).unwrap();
-    let mut qtype_bytes = bincode::serialize(&0x0001).unwrap();
-    let mut qclass_bytes = bincode::serialize(&0x0001).unwrap();
+    let mut qtype_bytes = bincode::serialize(&one_flag).unwrap();
+    let mut qclass_bytes = bincode::serialize(&one_flag).unwrap();
 
     let mut questions_bytes = vec![0; cname_bytes.len() + 4];
 
@@ -95,19 +113,18 @@ fn build_message(cname: String) -> DNSMessage{
     // message.append(&mut header_bytes);
     // message.append(&mut questions_bytes);
 
-    let message = DNSMessage{
+    let message = DNSMessage {
         header: header_bytes,
         payload: questions_bytes,
         answers_pr: 0,
         authority_rr: 0,
-        additional_pr: 0
+        additional_pr: 0,
     };
 
     message
 }
 
 fn transform_cname(cname: String) -> String {
-
     let split = cname.split(".");
     let mut labels: String = "".to_owned();
     for s in split {
